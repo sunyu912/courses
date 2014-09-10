896964c1ad0ea4ed3e6bf7b5e83400ded88b444b
Yu Sun <yu.sun.cs@gmail.com>
2014-09-10 09:28:33 -0700

    Update the feedback

--- course-monitor/src/main/java/io/yusun/course/controller/WebController.java
+++ course-monitor/src/main/java/io/yusun/course/controller/WebController.java
@@ -6,7 +6,9 @@ import io.yusun.course.repositories.StatusRepository;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,16 @@ public class WebController {
 		repository.save(courseStatus);		
 		return "OK";
 	}
+	
+	@RequestMapping(value = "/status2")
+	public String addFeedback(@RequestParam("data") String status) {
+		CourseStatus courseStatus = new CourseStatus(status, "");		
+		repository.save(courseStatus);		
+		return "OK";
+	}
 
+	@RequestMapping(value = "/ping", method = RequestMethod.GET)
+	public String healthCheck() {			
+		return "OK";
+	}
 }
