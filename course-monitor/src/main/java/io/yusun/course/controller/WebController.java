85138de9b9b0ae9efe118efdb41e490766fcf937
Yu Sun <yu.sun.cs@gmail.com>
2014-09-30 22:14:34 -0700

    New site

--- course-monitor/src/main/java/io/yusun/course/controller/WebController.java
+++ course-monitor/src/main/java/io/yusun/course/controller/WebController.java
@@ -1,12 +1,18 @@
 package io.yusun.course.controller;
 
 import io.yusun.course.data.CourseStatus;
+import io.yusun.course.data.StatManager;
 import io.yusun.course.repositories.StatusRepository;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,13 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
 
+	private Logger logger = LoggerFactory.getLogger(WebController.class);
+	
 	@Autowired
 	private StatusRepository repository;	
 
 	@RequestMapping(value = "/status")
 	public String addFeedback(@RequestParam("data") String status,
 			HttpServletRequest request) {
-		CourseStatus courseStatus = new CourseStatus(status, request.getRemoteAddr());		
+		CourseStatus courseStatus = new CourseStatus(status, request.getRemoteAddr());
 		repository.save(courseStatus);		
 		return "OK";
 	}
@@ -36,5 +44,46 @@ public class WebController {
 	@RequestMapping(value = "/ping", method = RequestMethod.GET)
 	public String healthCheck() {			
 		return "OK";
+	}
+	
+	
+	@RequestMapping(value = "reset", method = RequestMethod.GET)
+    public void resetGroup(
+            HttpServletResponse response) throws Exception {
+	    StatManager.get().resetMap();
+	    
+        response.setStatus(200);
+        response.getWriter().write("success");
+    }
+	
+	@RequestMapping(value = "stat", method = RequestMethod.GET)
+	public Map<String, Integer> getStat() {
+		return StatManager.get().getStatMap();
+	}
+	
+	@RequestMapping(value = "stat/view", method = RequestMethod.GET)
+	public Map<String, Integer> getStatView() {
+		return StatManager.get().getStatMap();
+	}
+	
+	@RequestMapping(value = "count", method = RequestMethod.GET)
+	public int getCount() {
+		return StatManager.get().getTotalSubmission();
+	}	
+	
+	@RequestMapping(value = "submit", method = RequestMethod.GET)
+	public void submitAnswer(
+			@RequestParam(value = "answer", required = true) String answers,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+	    logger.info("Request from session ID: " + request.getSession().getId() + " answer: " + answers);
+	    if (answers == null || answers.isEmpty()) {
+	    	return;
+	    }
+	    
+	    String userId = request.getSession().getId();
+	    StatManager.get().addSubmission(userId, Arrays.asList(answers.split(",")));
+	    
+	    response.setContentType("text/html; charset=utf-8");
+	    response.setStatus(200);        
 	}
 }
---
+++ course-monitor/src/main/java/io/yusun/course/data/StatManager.java
@@ -0,0 +1,75 @@
+package io.yusun.course.data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StatManager {
+	
+	private Map<String, List<String>> statMap = new HashMap<String, List<String>>();
+	private List<String> optionList = new ArrayList<String>(){{
+		add("A");
+		add("B");
+		add("C");
+		add("D");
+		add("E");
+		add("F");
+		add("G");
+		add("H");
+		add("I");
+	}};
+	
+	private StatManager() {
+		
+	}
+	
+	private static StatManager instance;
+	
+	public static StatManager get() {
+		if (instance == null) {
+			instance = new StatManager();
+		}
+		return instance;
+	}
+	
+	public void resetMap() {
+		statMap = new HashMap<String, List<String>>();
+	}
+	
+	public void addSubmission(String userId, List<String> answers) {
+		statMap.put(userId, answers);
+	}
+	
+	public int getTotalSubmission() {
+		if (statMap.keySet() != null) {
+			return statMap.keySet().size();
+		} else {
+			return 0;
+		}
+	}
+	
+	public Map<String, Integer> getStatMap() {
+		Map<String, Integer> countMap = new HashMap<String, Integer>();
+		for(String option : optionList) {
+			countMap.put(option, 0);
+		}
+		
+		for(List<String> answerList : statMap.values()) {
+			for(String answer : answerList) {
+				if (optionList.contains(answer)) {
+					Integer count = countMap.get(answer);
+					if (count == null) {
+						count = 0;
+					}
+					count++;
+					countMap.put(answer, count);
+				}
+			}
+		}
+		
+		Map<String, Integer> treeMap = new TreeMap<String, Integer>(countMap);
+		return treeMap;
+	}
+}
